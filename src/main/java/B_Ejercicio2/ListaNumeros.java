2f1a9807753da8aa18562745eed97773c4c0682f
jsepucas <jsepucas@myuax.com>
2024-05-07 13:37:00 +0200

    Commits

--- src/main/java/B_Ejercicio2/ListaNumeros.java
+++ src/main/java/B_Ejercicio2/ListaNumeros.java
@@ -5,3 +5,30 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
+public class ListaNumeros {
+    private List<Double> arrayList;
+    private List<Double> linkedList;
+
+    public ListaNumeros() {
+        arrayList = new ArrayList<>();
+        linkedList = new LinkedList<>();
+        llenarListas();
+    }
+
+    private void llenarListas() {
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            double valorAleatorio = random.nextDouble() * 100;  // Genera un número real aleatorio entre 0 y 100
+            arrayList.add(valorAleatorio);
+            linkedList.add(valorAleatorio);
+        }
+    }
+
+    public List<Double> getArrayList() {
+        return arrayList;
+    }
+
+    public List<Double> getLinkedList() {
+        return linkedList;
+    }
+}
